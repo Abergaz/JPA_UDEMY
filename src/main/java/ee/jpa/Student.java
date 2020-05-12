2326d1736ce6301d9cc57adf9d1a56ea82cc65ea
Abergaz <zagreba@gmail.com>
2020-05-12 20:13:34 +0300

    Урок 9. Авто инкремент Можно не использовать @GeneratedValue если в таблице настроить для стобца auto increment

--- src/main/java/ee/jpa/Student.java
+++ src/main/java/ee/jpa/Student.java
@@ -6,24 +6,11 @@ import java.util.Date;
 @Entity //Помечаем класс как запись такблицы и все его свойства - это столбцы
 @Table(name = "Students")//Указываем как будет называться таблица для @Entity данного класс
 
-//@SecondaryTable("Adresses") Позволяет часть данных класса выносить в другую таблицу, используется совместно с @Column(table="")
 @SecondaryTables({@SecondaryTable(name="Adresses"), @SecondaryTable(name="Countrys")})//Вынос даных в несколько таблиц
-
-@SequenceGenerator(name = "mySeq", initialValue = 1, allocationSize = 100)
 public class Student {
     //уникальный идентификатор @Id казатель первичного ключа Primary key, может быть над несколькими полями(составной первичный ключ)
     @Id
-    /** генериться автоматически уникальное значение
-     * @GeneratedValue, указывается стратегия и используемый генератор если нужен свой @TableGenerator или @SequenceGenerator
-     * GenerationType.AUTO - работает автоматически учитывая специфику СУДБ, выбирает сппособ ниже, для MSSQL - TABLE
-     * GenerationType.IDENTITY - не для всех СУБД
-     * GenerationType.SEQUENCE - не для всех СУБД (например MySQL не поддерживает)
-     * GenerationType.Table - создается отдельная табличка для генерации значений, например hibernate_sequence
-     * generator="имя" - можно настоить отдельную табличку с помощью @TableGenerator
-     */
-    @TableGenerator(name = "myGen",table = "mySequence",initialValue = 10,allocationSize = 100)//Настраиваем табичку для генерации первичных ключей
-    @GeneratedValue(strategy = GenerationType.TABLE,generator = "myGen") //Для стратегии Table указываем имя @TableGenerator
-    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mySeq") //Для стратегии SEQUENCE указываем имя @SequenceGenerator
+    /**  Можно не использовать @GeneratedValue, если в таблице настроить auto_increment для данного столбца*/
     int id;
     String name;
     int age;
